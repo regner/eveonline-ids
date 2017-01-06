95b4f8dac747d72904e5096b6d00b9dba2a1c703
Regner Blok-Andersen <shadowdf@gmail.com>
2017-01-06 06:21:19 +0000

    derp

--- crawler.py
+++ crawler.py
@@ -1,93 +1,151 @@
 
-import os
+import json
 import logging
-import argparse
-import requests
-import subprocess
+import asyncio
 
-from time import sleep
+from aiohttp import ClientSession
 
 logging.basicConfig(level=logging.INFO)
 
+PER_REQUEST = 300
+INFO = {
+    'characters': {
+        'url': 'https://esi.tech.ccp.is/latest/characters/names/',
+        'parameter': 'character_id',
+        'ranges': [
+            # [   90_000_000,    97_999_999],
+            [  100_000_000,   249_999_999],
+            # [  250_000_000,   499_999_999],
+            # [  500_000_000,   749_999_999],
+            # [  750_000_000,   999_999_999],
+            # [1_000_000_000, 1_249_999_999],
+            # [1_250_000_000, 1_499_999_999],
+            # [1_500_000_000, 1_749_999_999],
+            # [1_750_000_000, 1_999_999_999],
+            # [2_000_000_000, 2_111_999_999],
+            # [2_100_000_000, 2_111_999_999],
+            # [2_112_000_000, 2_119_999_999],  # Actually goes a lot further but will do for now
+        ],
+    },
+    # 'corporations': {
+    #     'url': 'https://esi.tech.ccp.is/latest/corporations/names/',
+    #     'parameter': 'corporation_id',
+    #     'ranges': [
+    #         [   98_000_000,    98_999_999],
+    #         [  100_000_000, 2_099_999_999],
+    #     ],
+    # },
+    # 'alliances': {
+    #     'url': 'https://esi.tech.ccp.is/latest/alliances/names/',
+    #     'parameter': 'alliance_id',
+    #     'ranges': [
+    #         [   99_000_000,    99_999_999],
+    #         [  100_000_000, 2_099_999_999],
+    #     ],
+    # },
+}
 
-def main(lookup: str, starting: int, ending: int):
-    # URL Setup
-    if lookup == 'corporations':
-        url = 'https://esi.tech.ccp.is/latest/corporations/names/'
-        parameter_name = 'corporation'
 
-    else:
-        url = 'https://esi.tech.ccp.is/latest/characters/names/'
-        parameter_name = 'character'
+def get_chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
 
-    logging.info(f'Using URL {url}')
 
-    # File Setup
-    out_file = f'{lookup}/{lookup}_ids_{starting}_{ending}.txt'
+def step_size(start, stop):
+    return int((stop - start) / PER_REQUEST)
 
-    # Adjust ID Range Start
-    if os.path.isfile(out_file):
-        starting = int(subprocess.check_output(['tail', '-1', out_file]))
 
-    # Some Extra Defaults
-    per_request = 300
-    sleep_time = 10
+def get_request_bundles():
+    bundles = []
 
-    default_parameters = {
-        'datasource': 'tranquility'
+    for id_type in INFO.keys():
+        base_url = INFO[id_type]['url']
+
+        for r in INFO[id_type]['ranges']:
+            start = r[0]
+            stop = r[1]
+
+            chunks = get_chunks(range(start, stop), PER_REQUEST)
+            
+            for chunk in chunks:
+                bundles.append((base_url, INFO[id_type]['parameter'], chunk))
+
+    return bundles
+
+
+async def fetch(session, url, parameter, chunk):
+    params = {
+        'datasource': 'tranquility',
+        f'{parameter}s': ','.join(str(x) for x in chunk),
     }
 
-    headers = {
-        'User-Agent': '<3 Regner <3'
-    }
+    start = chunk[0]
+    stop = chunk[-1]
 
-    # GO TIME!
+    logging.info(f'Making request for {parameter}s with chunk {start}-{stop}.')
+
     while True:
-        if starting > ending:
-            logging.info('Seems we are trying to start but already have everything...')
-            break
+        try:
+            async with session.get(url, params=params) as response:
+                if response.status == 200:
+                    return await response.read()
 
-        logging.info(f'Going with start of {starting:,}.')
-        character_ids = [str(x) for x in range(starting, starting + per_request)]
+        except:
+            pass
 
-        params = default_parameters.copy()
-        params[f'{parameter_name}_ids'] = ','.join(character_ids)
-
-        response = requests.get(url, params=params, headers=headers)
-
-        if response.status_code != requests.codes.ok:
-            logging.info(f'Had bad status code, sleeping for {sleep_time} and trying again.')
-            logging.info(f'Status code was: {response.status_code}')
-            logging.info(f'Message was: {response.text}')
-            sleep(sleep_time)
-            continue
-
-        data = response.json()
-        logging.info(f'Got good response with {len(data)} {lookup}.')
-        combined_data = '\n'.join(str(x[f'{parameter_name}_id']) for x in data)
-
-        if len(data) > 0:
-            with open(out_file, 'a') as f:
-                f.write('\n')
-                f.write(combined_data)
-
-        starting += per_request
-
-        if starting > ending:
-            break
+        await asyncio.sleep(1)
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Crawl some character IDs.')
-    parser.add_argument('--starting', type=int, help='Starting ID.', default=90_000_000)
-    parser.add_argument('--ending', type=int, help='Ending ID.', default=97_999_999)
-    parser.add_argument(
-        '--lookup',
-        type=str,
-        help='Valid values: "characters", "corporations", "alliances". Defaults to "characters"',
-        default='characters'
-    )
 
-    args = parser.parse_args()
+async def handle_response(response, parameter, chunk):
+    data = json.loads(response.decode('utf-8'))
 
-    main(args.lookup, args.starting, args.ending)
+    if len(data) > 0:
+        start = chunk[0]
+        stop = chunk[-1]
+
+        logging.info(f'Got {len(data)} IDs in range {start}-{stop}.')
+
+        
+        range_start = start - (start % 1_000_000)
+
+        out_file = f'test/{parameter}s_{range_start:_}.txt'
+        combined_data = '\n'.join(str(x[parameter]) for x in data)
+
+        with open(out_file, 'a') as f:
+            f.write('\n')
+            f.write(combined_data)
+
+
+
+async def bound_fetch(sem, session, url, parameter, chunk):
+    async with sem:
+        response = await fetch(session, url, parameter, chunk)
+        await handle_response(response, parameter, chunk)
+
+
+async def run():
+    tasks = []
+    sem = asyncio.Semaphore(10000)
+
+    logging.info('Building bundles...')
+    request_bundles = get_request_bundles()
+
+    logging.info('Creating async tasks...')
+    headers = {
+        'User-Agent': '<3 Regner <3',
+    }
+
+    async with ClientSession(headers=headers) as session:
+        for rb in request_bundles:
+            task = asyncio.ensure_future(bound_fetch(sem, session, rb[0], rb[1], rb[2]))
+            tasks.append(task)
+
+        responses = asyncio.gather(*tasks)
+        await responses
+
+
+loop = asyncio.get_event_loop()
+future = asyncio.ensure_future(run())
+loop.run_until_complete(future)
+
